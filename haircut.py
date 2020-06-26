702ee4a5ac8cb33e9a42f7ecf8a36516ddd3534a
Grant Hutchinson <grant@splorp.com>
2020-06-26 15:05:31 -0600

    Entabbed all the spacings

--- haircut.py
+++ haircut.py
@@ -23,57 +23,57 @@ HAIRCUTS = "https://splorp.com/about/haircut/"
 MONTHS = 'january february march april may june july august september october november december'.split()
 
 def fetch_dates():
-  soup = BeautifulSoup(urllib2.urlopen(HAIRCUTS).read())
-  return [span.string for span in soup.findAll('span', {"class": "dtstart"})]
+	soup = BeautifulSoup(urllib2.urlopen(HAIRCUTS).read())
+	return [span.string for span in soup.findAll('span', {"class": "dtstart"})]
 
 def get_dates():
-    dates = []
-    for d in fetch_dates():
-        day, month, year = d.lower().split()
-        try:
-            date = datetime.date(int(year), int(MONTHS.index(month))+1, int(day))
-        except ValueError, e:
-            print '*** uh oh: %s' % e
-            continue
-        dates.append(date)
-    dates.sort()
-    return dates
+	dates = []
+	for d in fetch_dates():
+		day, month, year = d.lower().split()
+		try:
+			date = datetime.date(int(year), int(MONTHS.index(month))+1, int(day))
+		except ValueError, e:
+			print '*** uh oh: %s' % e
+			continue
+		dates.append(date)
+	dates.sort()
+	return dates
 
 def get_deltas(dates):
-    deltas = []
-    mx = None
-    mx_delta = None
-    mn = None
-    mn_delta = None
-    for idx in range(0, len(dates)):
-        if idx > 0:
-            days = (dates[idx] - dates[idx-1]).days
-            if mx_delta is None or mx_delta < days:
-                mx_delta = days
-                mx = (dates[idx-1], dates[idx])
-            if mn_delta is None or mn_delta > days:
-                mn_delta = days
-                mn = (dates[idx-1], dates[idx])
-            deltas.append(days)
-    return mn, mx, deltas
-    
+	deltas = []
+	mx = None
+	mx_delta = None
+	mn = None
+	mn_delta = None
+	for idx in range(0, len(dates)):
+		if idx > 0:
+			days = (dates[idx] - dates[idx-1]).days
+			if mx_delta is None or mx_delta < days:
+				mx_delta = days
+				mx = (dates[idx-1], dates[idx])
+			if mn_delta is None or mn_delta > days:
+				mn_delta = days
+				mn = (dates[idx-1], dates[idx])
+			deltas.append(days)
+	return mn, mx, deltas
+	
 if __name__ == '__main__':
-    STR = "%B %d, %Y"
-    dates = get_dates()
-    mn, mx, deltas = get_deltas(dates)
-    avg = (sum(deltas) / len(deltas))
-    today = datetime.date.today()
-    last = dates[-1]
-    next = (last + datetime.timedelta(days=avg))
-    print "Number of haircuts since %s: %d" % (dates[0].strftime("%B %d, %Y"), len(dates))
-    print "Average days between haircuts: %d" % avg
-    print "Shortest time between haircuts: %d days (%s to %s)" % ((mn[1]-mn[0]).days, mn[0].strftime(STR), mn[1].strftime(STR))
-    print "Longest time between haircuts: %d days (%s to %s)" % ((mx[1]-mx[0]).days, mx[0].strftime(STR), mx[1].strftime(STR))
-    if (today-last).days == 0:
-    	print "You got your haircut today. Awesome."
-    if (today-last).days > 0:
-    	print "Your last haircut was %s days ago. (%s)" % ((today-last).days, last.strftime(STR))
-    if (next-datetime.date.today()).days <= -1:
-    	print "You probably should’ve had a haircut %s days ago. (%s)" % (abs((next-datetime.date.today()).days), next.strftime(STR))
-    if (next-datetime.date.today()).days >= 1:
-    	print "You probably should get another haircut in about %s days. (%s)" % ((next-datetime.date.today()).days, next.strftime(STR))
+	STR = "%B %d, %Y"
+	dates = get_dates()
+	mn, mx, deltas = get_deltas(dates)
+	avg = (sum(deltas) / len(deltas))
+	today = datetime.date.today()
+	last = dates[-1]
+	next = (last + datetime.timedelta(days=avg))
+	print "Number of haircuts since %s: %d" % (dates[0].strftime("%B %d, %Y"), len(dates))
+	print "Average days between haircuts: %d" % avg
+	print "Shortest time between haircuts: %d days (%s to %s)" % ((mn[1]-mn[0]).days, mn[0].strftime(STR), mn[1].strftime(STR))
+	print "Longest time between haircuts: %d days (%s to %s)" % ((mx[1]-mx[0]).days, mx[0].strftime(STR), mx[1].strftime(STR))
+	if (today-last).days == 0:
+		print "You got your haircut today. Awesome."
+	if (today-last).days > 0:
+		print "Your last haircut was %s days ago. (%s)" % ((today-last).days, last.strftime(STR))
+	if (next-datetime.date.today()).days <= -1:
+		print "You probably should’ve had a haircut %s days ago. (%s)" % (abs((next-datetime.date.today()).days), next.strftime(STR))
+	if (next-datetime.date.today()).days >= 1:
+		print "You probably should get another haircut in about %s days. (%s)" % ((next-datetime.date.today()).days, next.strftime(STR))
